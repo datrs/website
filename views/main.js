ae3bb44dbb76952d52a8f3959c7dd63cd5891035
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-20 08:32:09 +1100

    docs

--- views/main.js
+++ views/main.js
@@ -3,22 +3,37 @@ var html = require('choo/html')
 var links = [
   {
     name: 'flat-tree',
-    description: 'A series of functions to map a binary tree to a list',
-    url: 'https://github.com/mafintosh/flat-tree-rs'
-  },
-  {
-    name: 'merkle-tree-stream',
-    description: 'A stream that generates a merkle tree based on the incoming data',
-    url: 'https://github.com/yoshuawuyts/merkle-tree-stream'
+    description: 'A series of functions to map a binary tree to a list. This is later used to create Merkle Trees. The first algorithm required to implement the Dat protocol.',
+    url: 'https://github.com/datrs/flat-tree'
   },
   {
     name: 'print-flat-tree',
-    description: 'Converts a flat-tree to a string',
-    url: 'https://github.com/yoshuawuyts/print-flat-tree'
+    description: 'Converts a flat-tree to a string on the command line. Useful to visualize the "flat-tree" data structure and give you a better sense of how it works.',
+    url: 'https://github.com/datrs/print-flat-tree'
+  },
+  {
+    name: 'merkle-tree-stream',
+    description: 'A stream that generates a merkle tree based on the incoming data. This is the part that converts data into hashes, and creates hashes of hashes for verification. This is needed to be able to create Dat archives, but not to read them.',
+    url: 'https://github.com/datrs/merkle-tree-stream'
+  },
+  {
+    name: 'random-access-storage',
+    description: 'An abstract interface to implement reading / writing bytes to different backends from different offsets.',
+    url: 'https://github.com/datrs/random-access-storage'
+  },
+  {
+    name: 'random-access-memory',
+    description: 'Continuously read,write to memory using random offsets and lengths. This is particularly useful for testing and prototyping.',
+    url: 'https://github.com/datrs/random-access-memory'
+  },
+  {
+    name: 'random-access-file',
+    description: 'Continuously read,write to disk using random offsets and lengths. This is useful for persisting archives to disk.',
+    url: 'https://github.com/datrs/random-access-file'
   },
   {
     name: 'sleep-protocol',
-    description: ' SLEEP Network Protocol protobuf definitions',
+    description: ' SLEEP Network Protocol protobuf definitions. Not needed until you start sending things down the wire.',
     url: 'https://github.com/yoshuawuyts/sleep-protocol'
   }
 ]
@@ -46,30 +61,18 @@ function view (state, emit) {
           <h1 class="f2">dat.rs</h1>
           <p>
             Rustlang implementation of the Dat Project protocols. The goal is to
-            eventually bind to C, WebAssembly, Swift and Java.
+            eventually bind to C, WebAssembly, Swift and Java to support most
+            native platforms.
           </p>
           <p>
             Not every part of Dat will be equally portable. Especially browsers
             and mobile platforms have limitations in regards to IO.
             Platform-specific solutions will be necessary.
           </p>
-          <article class="mt5">
-            <h2>Overview</h2>
-            <p>
-              Dat is built up out of several data structures, and wire formats.
-              At the lowest level there's the flat-tree module, which can store
-              a merkle tree on disk.
-            </p>
-            <p>
-              [encrypt each node, store it in the merkle tree stream]
-            </p>
-            <p>
-              [encryption itself]
-            </p>
-            <p>
-              [9 files, on disk-format]
-            </p>
-          </article>
+          <p>
+            But we're convinced we can pull it off. Get involved if this sounds
+            like fun to you; help is always welcome!
+          </p>
           <article class="mt5">
             <h2>Further Reading</h2>
             <ul>
@@ -85,12 +88,14 @@ function view (state, emit) {
           <h2>
             Repositories
           </h2>
-          ${links.map(link => html`
-            <div class="mb3">
+          ${links.map((link, index) => html`
+            <div class="mb3 pv3">
+              <strong>${index + 1}.</strong>
               <a href=${link.url}>
                 ${link.name}
               </a>
-              <span> - ${link.description}</span>
+              <br>
+              <span class="mt2">${link.description}</span>
             </div>
           `)}
         </section>
