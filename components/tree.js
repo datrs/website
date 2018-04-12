1e2e610314ca25404a95f4eee49545e743102a02
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-04-13 00:47:22 +0200

    init component

---
+++ components/tree.js
@@ -0,0 +1,12 @@
+var Component = require('choo/component')
+var html = require('choo/html')
+
+module.exports = class Tree extends Component {
+  render () {
+    return html`
+      <section>
+        hello
+      </section>
+    `
+  }
+}
--- views/main.js
+++ views/main.js
@@ -1,42 +1,6 @@
 var html = require('choo/html')
 
-var links = [
-  {
-    name: 'flat-tree',
-    description: 'A series of functions to map a binary tree to a list. This is later used to create Merkle Trees. The first algorithm required to implement the Dat protocol.',
-    url: 'https://github.com/datrs/flat-tree'
-  },
-  {
-    name: 'print-flat-tree',
-    description: 'Converts a flat-tree to a string on the command line. Useful to visualize the "flat-tree" data structure and give you a better sense of how it works.',
-    url: 'https://github.com/datrs/print-flat-tree'
-  },
-  {
-    name: 'merkle-tree-stream',
-    description: 'A stream that generates a merkle tree based on the incoming data. This is the part that converts data into hashes, and creates hashes of hashes for verification. This is needed to be able to create Dat archives, but not to read them.',
-    url: 'https://github.com/datrs/merkle-tree-stream'
-  },
-  {
-    name: 'random-access-storage',
-    description: 'An abstract interface to implement reading / writing bytes to different backends from different offsets.',
-    url: 'https://github.com/datrs/random-access-storage'
-  },
-  {
-    name: 'random-access-memory',
-    description: 'Continuously read,write to memory using random offsets and lengths. This is particularly useful for testing and prototyping.',
-    url: 'https://github.com/datrs/random-access-memory'
-  },
-  {
-    name: 'random-access-file',
-    description: 'Continuously read,write to disk using random offsets and lengths. This is useful for persisting archives to disk.',
-    url: 'https://github.com/datrs/random-access-file'
-  },
-  {
-    name: 'sleep-protocol',
-    description: ' SLEEP Network Protocol protobuf definitions. Not needed until you start sending things down the wire.',
-    url: 'https://github.com/yoshuawuyts/sleep-protocol'
-  }
-]
+var Tree = require('../components/tree')
 
 module.exports = view
 
@@ -72,16 +36,7 @@ function view (state, emit) {
           <h2 class="f5 bt b--gray pt3">
             Repositories
           </h2>
-          ${links.map((link, index) => html`
-            <div class="mb3 pv3">
-              <strong>${index + 1}. </strong>
-              <a href=${link.url} class="white">
-                ${link.name}
-              </a>
-              <br>
-              <span class="mt2">${link.description}</span>
-            </div>
-          `)}
+          ${state.cache(Tree, 'tree').render()}
         </section>
       </main>
     </body>
